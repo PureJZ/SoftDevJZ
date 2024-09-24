9b8d2da3eaf6bc3dbb8e5de1f1e710acfbf55b2b
PureJZ <jzeng246@gmail.com>
2024-09-24 11:46:33 -0400

    tried to return list to site but doesn't work because too many return

--- 09_softserve/app.py
+++ 09_softserve/app.py
@@ -22,8 +22,26 @@ def numbercruncher():
         job.append(dict1.get("Job Class"))
     job.pop()
     percentage.pop()
+    return job
     return(random.choices(job, weights=percentage))
+app2= Flask(__name__)
+@app2.route("/")
+def returnlist():
+    list1=[]
+    job=[]
+    print("the __name__ of this module is... ")
+    print(__name__)
+    with open('occupations.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            list1.append(row)
+    for dict1 in list1:
+        job.append(dict1.get("Job Class"))
+    job.pop()
+    return job
+
 
 if __name__ == "__main__":      # true if this file NOT imported
     app.debug = True            # enable auto-reload upon code change
     app.run()
+    app2.run()
