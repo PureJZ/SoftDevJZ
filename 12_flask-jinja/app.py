e57068ad9354223db7d7347d1af6205dfbcbbb09
PureJZ <jzeng246@gmail.com>
2024-09-27 11:44:40 -0400

    added comments

--- 12_flask-jinja/app.py
+++ 12_flask-jinja/app.py
@@ -5,18 +5,18 @@
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Q0:
-<your response here>
 Removing render_template will make the function test_tmplt() not render the template as expected
-
+NameError: name 'render_template' is not defined
 Q1:
-<your response here>
 http://127.0.0.1:5000/my_foist_template
 Q2:
-<your response here>
-First argument is name of the template file to be rendered.
-Second is a variable being passed to the template
-Third is another variable passed to the template
+First argument is name of the template file to be rendered. 
+Second is a variable being passed to the template, this is the title of the page
+Third is another variable passed to the template, the template displays each element of the list on a new line on the site
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+The {% %} allows for python syntax in html
+This is present in both starting and ending the for loop that prints each elemnt of the list
+However, "item" is in double brackets
 """
 
 
@@ -25,7 +25,7 @@ Third is another variable passed to the template
 # Q0: What will happen if you remove render_template from the following statement?
 # (log prediction before executing...)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
