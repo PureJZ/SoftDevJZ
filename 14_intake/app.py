cf6c185da679236b84c1b03c4be6b4140e0b9876
PureJZ <jzeng246@gmail.com>
2024-10-09 10:55:29 -0400

    added 15

--- 14_intake/app.py
+++ 14_intake/app.py
@@ -39,38 +39,39 @@ PROTIP: Insert your own in-line comments
    understand what is going on.
 '''
 
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/",methods=['GET'])
 def disp_loginpage():
     ### All of these print statements print to the terminal
     
-    #print(testmod0.goo())
-    #print("\n\n\n")
-    #print("***DIAG: this Flask obj ***")
-    #print(app)
-    #print("***DIAG: request obj ***")
-    #print(request)
-    #print("***DIAG: request.args ***")
-    #print(request.args)
+    print(testmod0.goo())
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
     #print("***DIAG: request.args['username']  ***")
     #print(request.args['username'])
-    #print("***DIAG: request.headers ***")
-    #print(request.headers)
+    print("***DIAG: request.headers ***")
+    print(request.headers)
     return render_template( 'login.html' )
 
 
-@app.route("/auth") # , methods=['GET', 'POST'])
+@app.route("/auth", methods=['GET']) # , methods=['GET', 'POST'])
 def authenticate():
-    #print("\n\n\n")
-    #print("***DIAG: this Flask obj ***")
-    #print(app)
-    #print("***DIAG: request obj ***")
-    #print(request)
-    #print("***DIAG: request.args ***")
-    #print(request.args)
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
     #print("***DIAG: request.args['username']  ***")
     #print(request.args['username'])
-    #print("***DIAG: request.headers ***")
+    print("***DIAG: request.headers ***")
     print(request.headers)
+    print(request.args['username'])
     return "Waaaa hooo HAAAH"  #response to a form submission
 
 
