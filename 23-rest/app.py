dd842c3943a54c7b70c6c117a25300861a5ec84c
PureJZ <jzeng246@gmail.com>
2024-11-20 11:47:10 -0500

    part of 23

---
+++ 23-rest/app.py
@@ -0,0 +1,11 @@
+import flask from Flask , render_template
+import requests
+import json
+
+app=Flask(__main__)
+
+@app route('/', methods =["GET", "POST"])
+def index():
+    a=requests.get('https://api.nasa.gov/planetary/apod?api_key=i8i0oQRp50cOmZRCfMT6o7PgdI1xkNK9bCmNBAno')
+    b=a.json()
+    return render_template('index.html',l=b['hdurl'])
