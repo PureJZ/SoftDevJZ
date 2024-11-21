b371efa83ae5194252ea5c9657b2488b7f8d22cd
Jackie Zeng <80283862+PureJZ@users.noreply.github.com>
2024-11-21 00:48:15 -0500

    Turn on debug

--- 23-rest/app.py
+++ 23-rest/app.py
@@ -9,3 +9,6 @@ def index():
     a=requests.get('https://api.nasa.gov/planetary/apod?api_key=i8i0oQRp50cOmZRCfMT6o7PgdI1xkNK9bCmNBAno')
     b=a.json()
     return render_template('index.html',l=b['hdurl'])
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
