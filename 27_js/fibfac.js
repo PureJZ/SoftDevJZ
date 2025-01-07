2204020e1bdb741ca46f0ab63bc2e2e06022d8e9
Jackie Zeng <jzeng246@gmail.com>
2025-01-06 23:33:15 -0500

    27

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -18,7 +18,11 @@ let fact = function(n){
 
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
-
+console.log(fact(1)); // "...should be 1"
+console.log(fact(2)); // "...should be 2"
+console.log(fact(3)); // "...should be 6"
+console.log(fact(4)); // "...should be 24"
+console.log(fact(5)); // "...should be 120"
 
 //-----------------------------------------------------------------
 
@@ -38,5 +42,10 @@ let fib = function(n){
 }
 //TEST CALLS
 // (writing here can facilitate EZer copy/pasting into dev console now and later...)
-
+console.log(fib(0)); // "...should be 0"
+console.log(fib(1)); // "...should be 1"
+console.log(fib(2)); // "...should be 1"
+console.log(fib(3)); // "...should be 2"
+console.log(fib(4)); // "...should be 3"
+console.log(fib(5)); // "...should be 5"
 //=================================================================
